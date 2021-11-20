c2f16e4d2634609c4c8e46c98e63377d79da75a0
Kaylode <kayp.kieran@gmail.com>
2021-11-20 15:15:39 +0700

    add sign guidance

--- RPGShooter/Assets/Scripts/Character/NPC/NPC.cs
+++ RPGShooter/Assets/Scripts/Character/NPC/NPC.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class NPC : Collidable
 {
     public Dialog dialog;
-    public bool interacble = false;
+    protected bool interacble = false;
 
-    protected void OnCollisionStay2D(Collision2D collision)
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.interacble = true;
+        }
+    }
+
+    protected void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.interacble = false;
+        }
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
